48cf933ec4f371d9f376fe0ca877c7b7c515f9b0
Maria2421 <mp.becerra2424@gmail.com>
2025-11-10 12:22:49 -0500

    ui final

--- Assets/Scripts/changeCanva.cs
+++ Assets/Scripts/changeCanva.cs
@@ -2,44 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class changeCanva : MonoBehaviour
 {
-    [Header("Elementos del Canvas")]
-    public Button boton;          // Botón que se activará
-    public GameObject textoNuevo; // Texto que se activará
-    public GameObject textoViejo; // Texto que se desactivará
-
-    [Header("Tiempo de espera (segundos)")]
-    public float tiempoEspera = 3f;
-
-    private bool iniciado = false;
-
-    void OnEnable()
+    public void cambiarEscena()
     {
-        // Solo iniciar si el Canvas acaba de activarse
-        if (!iniciado)
-        {
-            iniciado = true;
-            StartCoroutine(ActivarDespuesDeTiempo());
-        }
-    }
-
-    IEnumerator ActivarDespuesDeTiempo()
-    {
-        // Estado inicial al activarse el Canvas
-        if (boton != null) boton.gameObject.SetActive(false);
-        if (textoNuevo != null) textoNuevo.SetActive(false);
-        if (textoViejo != null) textoViejo.SetActive(true);
-
-        // Esperar el tiempo indicado
-        yield return new WaitForSeconds(tiempoEspera);
-
-        // Cambiar los estados
-        if (boton != null) boton.gameObject.SetActive(true);
-        if (textoNuevo != null) textoNuevo.SetActive(true);
-        if (textoViejo != null) textoViejo.SetActive(false);
+        SceneManager.LoadScene("3dEnvironment");
     }
 }
 
